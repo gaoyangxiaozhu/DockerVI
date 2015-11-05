d7a7b4be3adcc8cf0e54edc6d5e6dde6a0189104
gaoyangxiaozhu <1456543560@qq.com>
2015-11-05 13:39:24 +0800

    add restart , start , stop function for containers

--- build/js/common.js
+++ build/js/common.js
@@ -8,16 +8,22 @@ $(function(){
             breadcrumb.find('li:nth-child(2)').text(secondText);
             breadcrumb.find('li:nth-child(3)').text(thirdText);
         }
+        function update_page_header(h1Text, smallText){
+            pageHeader = $('.page-header');
+            pageHeader.find('span').text(h1Text);
+            pageHeader.find('small').text(smallText);
+        }
         sectionId = that.data('id');
         if(!that.hasClass('active')){
             // 更新active
 
             that.addClass('active')
-            .siblings('li').removeClass('active')
+            .siblings('li').removeClass('active').end()
             .parents('.submenu').parents('li')
             .addClass('active open')
             .siblings('li').removeClass('active open')
-            .find('li').removeClass('active')
+            .find('li').removeClass('active').end()
+            .find('.submenu:visible').prev('a').trigger('click')
 
             //更新显示模块
 
@@ -28,6 +34,10 @@ $(function(){
             finalLiText = that.find('a').text()
             secondLiText = that.parents('.submenu').data('info')
             update_breadcrumb(secondLiText, finalLiText);
+
+            //更新page-header
+            dataInfoDetail = that.data('info-detail');
+            update_page_header(secondLiText, dataInfoDetail);
 
        }
     }
--- build/js/main.js
+++ build/js/main.js
@@ -55,27 +55,34 @@ function show_dialog(fn){
          that.jqGrid('setRowData', ids[i], {Delete: delBtn});
        }
      }
-
+     //ajax err 回调函数
+     function err(xhr, textStatus, errorThrown){
+         console.log(textStatus+' '+errorThrown);
+     }
       function Containers(){
 
         var element = $("#cgrid");
         var elemntPager = $("#cgrid-page");
+        var urlPrefix = 'http://127.0.0.1:8080/containers/';
         // container grid选项
         var option = {
             datatype: 'local',
-            colNames: [' ', 'Image', 'Id', 'Created', 'Status'],
+            colNames: [' ', 'Image', 'Id', 'Created', 'Status', 'Action'],
             colModel: [
-                {name:'Delete', width:70, align:"center"},
-                {name: 'Image', width: 150},
+                {name:'Delete', width:70, align:"center", fixed:true, sortable:false, resize:false},
+                {name: 'Image', width: 200},
                 {name: 'Id', width: 200},
                 {name: 'Created', width: 200},
-                {name: 'Status', width: 180}
+                {name: 'Status', width: 100, fixed:true, sortable:false, resize:true, align: 'center', formatter: initial_state_label},
+                {name: 'Action', width:230, fixed:true, sortable:false, resize:true, align:'center', formatter:initial_dropbutton_action}
             ],
             rowNum:30,
             rowList:[10,20,30],
             pager: elemntPager,
             altRows:true,
             height: 'auto',
+            autowidth:true,
+            viewrecords: true,
             hoverrows: false,
             caption: 'Container List',
             gridComplete: function(){
@@ -85,7 +92,71 @@ function show_dialog(fn){
               that.find('.btn-del-cm').addClass('del-container-btn');
             }
         }
+        //获得container的运行状态
+        function get_state_container(stateStr){
+            var stateStr = $.trim(stateStr);
+            if(stateStr=='running'|| stateStr=='stop') return stateStr;
+            var re = /Exited/;
+            return stateStr? re.test(stateStr)? 'stop':'running' : 'error';
+        }
+        // update state contaier
+        function update_container_state(state){
 
+            var LabelHtmlTemplate= {
+                err: "<span class='label label-danger label-xlg'>Error</span>",
+                stop: "<span class='label label-xlg'>Stoped</span>",
+                running: "<span class='label label-success label-xlg'>Running</span>"
+
+            }
+            switch(get_state_container(state)){
+                case 'error': return LabelHtmlTemplate.err;
+                case 'running': return LabelHtmlTemplate.running;
+                case 'stop': return LabelHtmlTemplate.stop
+            }
+
+        }
+        //set container state : error, stop, running ..
+        function initial_state_label(cellvalue, options, rowObject){
+            return update_container_state(cellvalue);
+        }
+        // update dropdown menu for current container
+        function update_dropbutton_menu(id, status){
+            var dropdownMenuTemplate = {
+                noActionHead: "<div class='btn-group'>"+
+                                "<button class='btn btn-sm'>No Action</button>",
+                actionHead: "<div class='btn-group'>"+
+                                "<button class='btn btn-sm btn-info'>Some Action</button>"+
+                                "<button data-toggle='dropdown' class='btn btn-sm btn-info dropdown-toggle'><i class='icon-angle-down icon-only'></i></button>"+
+                                "<ul class='dropdown-menu dropdown-yellow'>",
+                noActionFooter :'</div>',
+                actionFooter: '</ul></div>',
+
+                stopState: function(id){
+                        return ["<a href='#' class='start-container' data-id='"+id+"'>"+"Start</a>"];
+                    },
+                runningState: function(id){
+                    return [
+                            "<a href='#' class='stop-container' data-id='"+id+"'>"+"Stop</a>",
+                            "<a href='#' class='restart-container' data-id='"+id+"'>"+"Restart</a>"
+                        ];
+                }
+
+            }
+            // 通过li元素拼接菜单数组
+            function join_html_by_li(menuArray){
+                return '<li>'+menuArray.join('</li><li>')+'</li>';
+            }
+
+            switch(get_state_container(status)){
+                case 'error': return dropdownMenuTemplate.noActionHead+dropdownMenuTemplate.noActionFooter;
+                case 'stop': return dropdownMenuTemplate.actionHead+join_html_by_li(dropdownMenuTemplate.stopState(id))+dropdownMenuTemplate.actionFooter;
+                case 'running': return dropdownMenuTemplate.actionHead+join_html_by_li(dropdownMenuTemplate.runningState(id))+dropdownMenuTemplate.actionFooter;
+            }
+        }
+        //initial dropdown-menu  in Containers for stop , pause , restart, start
+        function initial_dropbutton_action(cellvalue, options, rowObject){
+            return update_dropbutton_menu(rowObject['Id'], rowObject['Status']);
+        }
         var inspect =function(id){
 
         }
@@ -96,18 +167,43 @@ function show_dialog(fn){
               console.log('del failed');
             }
             var ajaxOption = {
-              url: 'http://127.0.0.1:8080/containers/'+id,
+              url: urlPrefix+id,
               type: 'DELETE',
-              success: function(data){
-                console.log('success');
-              },
-              error: function(xhr, textStatus, errorThrown){
-                  console.log(errorThrown);
-              }
+              success: function(data){ console.log('remove');},
+              error: err
             }
             $.ajax(ajaxOption);
           }
           show_dialog(do_remove);
+        }
+        var stop = function(id,fn){
+            var option ={};
+            var ajaxOption = {
+              url: urlPrefix+id+'/stop',
+              type: 'POST',
+              success: function(data){ fn();},
+              error: err
+            }
+            $.ajax(ajaxOption);
+
+        }
+        var restart = function(id,fn){
+            var ajaxOption = {
+              url: urlPrefix+id+'/restart',
+              type: 'POST',
+              success: function(data){fn()},
+              error: err
+            }
+            $.ajax(ajaxOption);
+        }
+        var start = function(id,fn){
+            var ajaxOption = {
+              url: urlPrefix+id+'/start',
+              type: 'POST',
+              success: function(data){fn();},
+              error: err
+            }
+            $.ajax(ajaxOption);
         }
         var init = function(){
           var that =element;
@@ -121,20 +217,21 @@ function show_dialog(fn){
                   data[i]['Created'] = get_format_date(data[i]['Created'])
                 }
                 option.data = data;
-                console.log(data);
-                console.log(that);
                 that.jqGrid(option);
             },
-            error: function(xhr, textStatus, errorThrown){
-              console.log(errorThrown);
-            }
+            error: err
           }
           $.ajax(ajaxOption);
         }
         return {
           init: init,
           inspect: inspect,
-          remove : remove
+          remove : remove,
+          start: start,
+          restart: restart,
+          stop: stop,
+          updateContainerState:update_container_state,
+          updateDropButtonMenu: update_dropbutton_menu
         }
 
       }
@@ -236,6 +333,52 @@ function show_dialog(fn){
             image.remove(id, rowId);
           }
 
-      })
+      });
+
+      function common_action_for_container_button(newState){
+          that = $(this);
+          currentActionTd= that.parents('td');
+          currentStateTd = currentActionTd.prev('td');
+          var id = that.data('id');
+          return option={
+              id:id,
+              //action remove api 调用并执行成功后的回调函数 主要用于更新state和更新菜单选项
+
+              succ: function succ(){
+                  currentStateTd.html(container.updateContainerState(newState));
+                  currentActionTd.html(container.updateDropButtonMenu(id, newState));
+              }
+          }
+
+      }
+    //   绑定stop container event
+
+      $(document).on('click', '.stop-container', function(e){
+            e.preventDefault();
+            option=common_action_for_container_button.call(this, 'stop');
+            // 调用 docker reomte api 停止 当前container
+            // succ为ajax成功后在success回调函数内部执行的函数
+
+            container.stop(option.id, option.succ);
+
+      });
+
+    //  绑定start container event
+    $(document).on('click', '.start-container', function(e){
+        e.preventDefault();
+        option =common_action_for_container_button.call(this, 'running');
+        //调用docker remote api 启动当前的container
+
+        container.start(option.id, option.succ);
+    });
+
+    //绑定restart container event
+    $(document).on('click', '.restart-container', function(e){
+        option=common_action_for_container_button.call(this, 'running');
+        // 调用docker remote api 重启当前的container
+
+        container.restart(option.id, option.succ);
+
+    })
    })();
 })
