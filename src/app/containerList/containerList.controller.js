ea0b4c47fe0a7721ee4e62c0b6f003d5b88ec59d
gaoyangxiaozhu <1456543560@qq.com>
2016-05-12 15:16:05 +0800

    modify src/app/**/*.js -- controller func for containerlist and imagelist

--- src/app/containerList/containerList.controller.js
+++ src/app/containerList/containerList.controller.js
@@ -1,79 +1,46 @@
 // container list page controller
 (function(){
     angular.module("dockerApp")
-    .controller('ContainerListCtrl', ['$scope', '$location', 'container', function($scope, $location, container){
-        var containers = [];
+    .controller('ContainerListCtrl', ['$scope', '$location', 'Container', function($scope, $location, container){
+        $scope.containerList = [];
 
-        //getSubList
-        $scope.getSubList= function(page){
-            var start= (page-1)*$scope.pageSize;
-            var end = start+$scope.pageSize;
-            $scope.containers = container.getSubList(start, end);
-
-            // 如果当前为空页 就返回到上一页显示
-            if($scope.containers.length===0){
-                if(page==1) return;
-                $scope.currentPage=page-1;
-                $scope.total = container.getLength();
-                $scope.getSubList($scope.currentPage);
-            }
-
-            //scroll to top
-            function scrollTop(){
-                $("html, body").animate({scrollTop: $("html, body").offset().top}, "fast")
-            }
-            t=setTimeout(scrollTop, 0);
-        };
-        //通过container.init()对容器列表进行初始化工作　
-        //主要是获取容器列表并赋值给全局变量_containers
-
-        container.init();
-
-        //遍历容器列表　如果当前容器是开启状态就分配资源收集模块　收集资源数据
-
-        container.resourceCollectForCurrentRunningContainers();
-
-        // container list获取成功后在回调函数中执行containre_list_init函数初始化scope.containers
-        // 默认containers只获得前10个数据
-        var container_list_init=function(data){
-            // 获取容器列表
-            $scope.currentPage=1;
-            $scope.pageSize =15;
-            $scope.total = data.length;
-            $scope.getSubList(1);
-
-
-        };
-        container.data(null, container_list_init);
-
-
-        // 运行容器
-        $scope.startContainer= function(currentContainer){
-
-            // 成功启动容器后执行的回调函数
-
-            var after_start_container= function(data){
-                currentContainer.Status = 'running';
-            };
-            container.start(currentContainer.Id, currentContainer['node_name'][1], after_start_container);
+        $scope.option = {
+            currentPage: 1,
+            itemPerPage : 10
         };
 
-        // 停止容器
-        $scope.stopContainer= function(currentContainer){
-            // 成功关闭容器后执行的回调函数
-            var after_stop_container = function(data){
-                currentContainer.Status = 'stop';
-            };
-            container.stop(currentContainer.Id, after_stop_container);
-        };
+        function doPaging(options){
+            $scope.isLoading = true;
+             //数量需要过滤
+             Blog.getContainerCount(options).then(function(result){
+                $scope.containerCount = result.count;
+                $scope.numPages = Math.ceil($scope.containerCount/$scope.options.itemsPerPage);
+             });
+            //获取列表
+            Blog.getContainerList(options).then(function(result){
+                $scope.isLoading = false;
+                $scope.blogList = result.data;
+            }).catch(function(){
+               $scope.isLoading = false;
+                $scope.containerList = [];
+            });
+        }
+        //初始化列表
+        doPaging($scope.option);
+
+        //加载更多
+       $scope.loadMore = function(page){
+           $scope.options.currentPage = page;
+           doPaging($scope.options, true);
+       };
+
         // 删除容器
         $scope.delContainer= function(currentContainer){
             // 删除成功则更新当前的containers
             function updateContainers(){
-                $scope.getSubList($scope.currentPage);
+                doPaging($scope.currentPage);
             }
-            console.log(currentContainer.Id);
-            container.remove(currentContainer.Id.slice(0,12), updateContainers);
+            Container.deleteContainer({_id : currentContainer.id }, updateContainers);
         };
     }]);
 })();
