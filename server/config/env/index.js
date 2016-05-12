f7d42647461fb063f6f5c17a738f2d9bced50a14
gaoyangxiaozhu <1456543560@qq.com>
2016-05-12 15:18:55 +0800

    commit config.js file for back-end

---
+++ server/config/env/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var path = require('path');
+var _ = require('lodash');
+var fs = require('fs');
+
+var all = {
+  env: process.env.NODE_ENV,
+  root: path.normalize(__dirname + '/../../..'),
+  port: process.env.PORT || 9000,
+  mongo: {
+    options: {
+      db: {
+        safe: true
+      }
+    }
+  },
+  seedDB: false,
+  secrets: {
+    session: 'docker-secret'
+  },
+  userRoles: ['user', 'admin'],
+  qiniu:{
+    app_key:"app_key",
+    app_secret:"app_secret",
+    domain:"domain",          //七牛配置域名
+    bucket:"bucket"           //七牛空间名称
+  },
+  github:{
+    clientID:"github",
+    clientSecret:"clientSecret",
+    callback:"/auth/github/callback"
+  },
+};
+
+var config = _.merge(all, require('./' + process.env.NODE_ENV + '.js') || {});
+//加载私有配置
+if (fs.existsSync(path.join(__dirname, 'private/index.js'))) {
+  config = _.merge(config, require(path.join(__dirname, 'private/index.js')) || {});
+}
+module.exports = config;
---
+++ server/config/express.js
@@ -0,0 +1,51 @@
+/**
+ * Express configuration
+ */
+
+'use strict';
+
+var express = require('express');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var errorHandler = require('errorhandler');
+var path = require('path');
+var config = require('./env');
+var passport = require('passport');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+var sessionMongo = mongoose.createConnection(config.mongo.suri, config.mongo.options)
+
+module.exports = function(app) {
+  var env = app.get('env');
+
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(multer());
+  app.use(methodOverride());
+  app.use(cookieParser());
+  app.use(session({
+    secret: config.secrets.session,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: sessionMongo }),
+    cookie: { maxAge: 60000 }
+  }));
+  app.use(passport.initialize());
+  if ('development' === env) {
+    app.use(errorHandler());
+  }else if ('test' === env) {
+    app.set('appPath', 'dist');
+    app.use(express.static(path.join(config.root, 'dist')));
+  }else{
+    app.set('appPath', 'dist');
+    app.use(express.static(path.join(config.root, 'dist')));
+    app.use(favicon(path.join(config.root, 'dist', 'favicon.ico')));
+  }
+};
