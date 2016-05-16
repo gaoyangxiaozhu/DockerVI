98cd2f5e98dc0ff1afaad064dd08e967144b5f7a
gaoyangxiaozhu <1456543560@qq.com>
2016-05-16 11:30:30 +0800

    modify cluster.controller.js for can get cluster resource info from back-end and show in front-end

--- src/app/cluster/cluster.controller.js
+++ src/app/cluster/cluster.controller.js
@@ -1,7 +1,12 @@
 // image list page controller
 (function(){
     angular.module("dockerApp.cluster")
-    .controller('clusterCtrl', ['$scope', '$location', function($scope, $location){
-
+    .controller('clusterCtrl', ['$scope', '$location', 'Cluster', function($scope, $location, Cluster){
+        $scope.clusterList =[];
+        Cluster.getClusterList().then(function(results){
+            $scope.clusterList = results.nodeArray;
+            $scope.containerNums = results.containerNums;
+            $scope.imageNums = results.imageNums;
+        });
     }]);
 })();
