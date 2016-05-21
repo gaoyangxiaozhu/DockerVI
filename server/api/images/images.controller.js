44d5875d118e271e6243c3ca4f67d2e84c22739c
gaoyangxiaozhu <1456543560@qq.com>
2016-05-21 20:24:59 +0800

    add getImageDetail function for images in back-end

--- server/api/images/images.controller.js
+++ server/api/images/images.controller.js
@@ -10,70 +10,83 @@ var request = require('../../components/superagent');
 var endpoint = require('../endpoint').SWARMADDRESS;
 
 
-var formatData =function(data){
-	  var images= [];
-	  var imageName, imageTag, repo;
-	  for(var index in data){
-		  var item = data[index];
-		  for(var index2 in item.RepoTags){
-			  var isPrivate = false;
-			  repo = item.RepoTags[index2];
-			  var imagePrefixStr = repo.slice(0, repo.lastIndexOf(':'));
-			  var image={};
-			  var imagePrefixStrArray= imagePrefixStr.split('/');
-			  //镜像标签
-			  image.tag = repo.slice(repo.lastIndexOf(':') + 1);
-			  if(imagePrefixStrArray.length == 1){
-				  // 说明是官方仓库的官方镜像
-				  image.repo = 'docker.io.com';
-				  image.username = 'docker';
-				  image.name = imagePrefixStrArray[0];
-			  }else{//说明不是官方镜像
-				  //RepoTags中的item可能为如"localhost:5000/busybox:latest"这种形式 此时为私有镜像
-				  var socket = imagePrefixStrArray[0].indexOf(':') >=0 ? true : false;
-				  //如果为true说明是私有仓库
-				  if(socket){
-					  image.repo = imagePrefixStrArray[0];
-					  if(imagePrefixStrArray.length == 2){
-						  //说明没有用户名　默认设置为gyyzyp
-						  image.username = 'gyyzyp';
-						  image.name = imagePrefixStrArray[1];
-					  }else{
-						  image.username = imagePrefixStrArray[1];
-						  image.name = imagePrefixStrArray[2];
-					  }
-					  isPrivate = true;
-				  }else{
-					  //否则为官方仓库的个人镜像
-					  image.repo = 'docker.io.com';
-					  image.username = imagePrefixStrArray[0];
-					  image.name = imagePrefixStrArray[1];
-				  }
-			  }
-			  if(isPrivate){
-				  break;
-			  }
-			 image.time = item.Created * 1000;
-   		   	 image.size = item.VirtualSize;
-
-			 var alreadyHas = false;
-		 	 //  如果当前镜像和已经存入的镜像相同 则遗弃
-		  	 for(var index3 in images){
-				 var oldImage = images[index3];
-				 if(oldImage.name.trim() == image.name.trim() && oldImage.tag.trim() == image.tag.trim()){
-					 alreadyHas = true;
-					 break;
-			  	}
-		  	}
-			if(!alreadyHas){
-				 images.push(image);
+var formatRepoTag = function(repoTag){
+	var isPrivate = false;
+	var repo = repoTag;
+	var imagePrefixStr = repo.slice(0, repo.lastIndexOf(':'));
+	var image={};
+	var imagePrefixStrArray= imagePrefixStr.split('/');
+	//镜像标签
+	image.tag = repo.slice(repo.lastIndexOf(':') + 1);
+	if(imagePrefixStrArray.length == 1){
+		// 说明是官方仓库的官方镜像
+		image.repo = 'docker.io.com';
+		image.username = 'docker';
+		image.name = imagePrefixStrArray[0];
+	}else{//说明不是官方镜像
+		//RepoTags中的item可能为如"localhost:5000/busybox:latest"这种形式 此时为私有镜像
+		var socket = imagePrefixStrArray[0].indexOf(':') >=0 ? true : false;
+		//如果为true说明是私有仓库
+		if(socket){
+			isPrivate = true;
+			image.repo = imagePrefixStrArray[0];
+			if(imagePrefixStrArray.length == 2){
+				//说明没有用户名　默认设置为gyyzyp
+				image.username = 'gyyzyp';
+				image.name = imagePrefixStrArray[1];
+			}else{
+				image.username = imagePrefixStrArray[1];
+				image.name = imagePrefixStrArray[2];
 			}
 
-		  }
-	  }
-	  return images;
-  };
+		}else{
+			//否则为官方仓库的个人镜像
+			image.repo = 'docker.io.com';
+			image.username = imagePrefixStrArray[0];
+			image.name = imagePrefixStrArray[1];
+		}
+	}
+	image.isPrivate = isPrivate ? true : false;
 
+	return image;
+
+};
+var formatSize =function(size){
+	// K M G T
+	switch (true){
+		case size < 900000000 : return (size / 1000 / 1000).toFixed(2) + 'M';
+		default: return (size / 1000 / 1000 / 1000).toFixed(2) + 'G';
+	}
+	return ;
+};
+var formatData =function(data){
+
+	var images= [];
+	var image ={};
+
+	if(!(Object.prototype.toString.call(data) === '[object Object]' || Object.prototype.toString.call(data) === '[object Array]')){
+		return {};
+	}
+	if(Object.prototype.toString.call(data) == '[object Object]'){
+		image = formatRepoTag(data.RepoTags[0]);
+		image.DockerVersion = data.DockerVersion;
+		image.id = data.Id.split(':')[1].slice(0, 10);
+		image.time = new Date(data.Created).getTime();
+		image.size = formatSize(data.VirtualSize);
+		return image;
+	}
+
+  	for(var index in data){
+	  	var item = data[index];
+		image = formatRepoTag(item.RepoTags[0]);
+		image.id = item.Id.split(':')[1].slice(0, 10);
+		image.time = item.Created * 1000;
+		image.size = formatSize(item.VirtualSize);
+		if(image.isPrivate) continue;
+		images.push(image);
+  	}
+  return images;
+ };
 exports.getImagesList = function(req, res){
 
 	var url = endpoint + '/images/json';
@@ -112,6 +125,28 @@ exports.getImagesCount = function(req, res){
 	}).fail(function(err){
 			res.send({'error_msg': 'error'});
 	});
+};
+exports.getImageDetail = function(req, res){
+	var id = req.params.id;
+	var url = endpoint + '/images/' + id + '/' + 'json';
+	request.get(url)
+	.then(function(response){
+			if(!response.ok){
+					throw new Error("error");
+			}
+			var _data = response.body;
+			try {
+				_data = formatData(_data);
+				res.send(_data);
+			} catch (e) {
+				throw new Error(e);
+			}
+
+
+	}).fail(function(err){
+			res.send({'error_msg': err.message});
+	});
+
 };
 exports.deleteImage = function(req ,res){
 
