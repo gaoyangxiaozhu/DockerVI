e79fcf04e61097c5fffeb27d4c130749524d5577
gaoyangxiaozhu <1456543560@qq.com>
2016-05-25 21:25:16 +0800

    add volume resource js

---
+++ src/app/components/resource/volume.js
@@ -0,0 +1,87 @@
+(function () {
+  'use strict';
+
+  angular.module('dockerApp.resources')
+  .factory('Volume', function($resource){
+      var volumesResource = $resource('/api/volumes/:id/:controller', {
+          id: '@_id'
+        },
+        {
+          //添加标签分类
+          getVolumesList: {
+              method: 'GET',
+              params: {
+                  controller:'getVolumesList'
+              }
+          },
+          getVolumesCount:{
+              method: 'GET',
+              params: {
+                  controller: 'getVolumesCount'
+              }
+          },
+          getVolumesDetail:{
+              method:'GET',
+              params:{
+                  controller: 'getVolumesDetail'
+              }
+          },
+          searchVolume:{
+              method:'GET',
+              params:{
+                  controller: 'searchVolume'
+              }
+          }
+        });
+      return {
+        getVolumesList : function(data, callback){
+            var cb = callback || angular.noop;
+          return volumesResource.getVolumesList(function(result) {
+                    return cb(result);
+                }, function(err) {
+                        return cb(err);
+                }).$promise;
+        },
+        getImagesCount : function(data, callback){
+            var cb = callback || angular.noop;
+            return volumesResource.getVolumesCount(function(result) {
+                          return cb(result);
+                  }, function(err) {
+                          return cb(err);
+                  }).$promise;
+        },
+        getVolumesDetail : function(data, callback){
+            var cb = callback || angular.noop;
+            return volumesResource.getVolumesDetail(data, function(result){
+                return cb(result);
+            }, function(err){
+                return cb(err);
+            }).$promise;
+        },
+        createNewVolume : function(data, callback){
+            var cb = callback || angular.noop;
+            return volumesResource.save(data, function(result){
+                return cb(result);
+            }, function(err){
+                return cb(err);
+            }).$promise;
+        },
+        searchVolume: function(data, callback){
+            var cb = callback || angular.noop;
+            return volumesResource.searchVolume(data, function(result){
+                return cb(result);
+            }, function(err){
+                return cb(err);
+            }).$promise;
+        },
+        deleteVolume: function(data,callback){
+            var cb = callback || angular.noop;
+            return volumesResource.remove(data, function(result) {
+                return cb(result);
+            }, function(err) {
+                return cb(err);
+            }).$promise;
+        }
+     };
+    });
+})();
