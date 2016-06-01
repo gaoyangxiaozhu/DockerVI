f94dbbfd54f4aa81ecfc462cfeafacc34afbc8a3
gaoyangxiaozhu <gyyzyp@163.com>
2016-06-01 21:20:59 +0800

    modify server/config

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -3,12 +3,19 @@
 // 生产环境配置
 // =================================
 module.exports = {
-  port:     process.env.PORT || 8100,
-  mongo: {
-    uri: 'mongodb://localhost/dockerGrand',
-    options: {
-      user:'user',          //生产环境用户名
-      pass:'pass'           //生产环境密码
+    port:     process.env.PORT || 8100,
+    mongo: {
+        suri: 'mongodb://0.0.0.0:32777/session-dev',
+        options: {
+            user:'user',          //生产环境用户名
+            pass:'pass'           //生产环境密码
+        }
+    },
+    mysql: {
+        host: '0.0.0.0',
+        port: '3333',
+        user: 'root',
+        password: '123123',
+        db:'docker'
     }
-  }
 };
