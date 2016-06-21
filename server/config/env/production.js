49f98ce738409a3efa6459f21acf544bd925ffd7
gaoyangxiaozhu <gyyzyp@163.com>
2016-06-21 20:00:33 +0800

    ignore dist

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -5,13 +5,13 @@
 module.exports = {
     port:     process.env.PORT || 8100,
     mongo: {
-        suri: 'mongodb://0.0.0.0:32777/session-dev',
+        suri: 'mongodb://0.0.0.0:32777/session-dev'
     },
     mysql: {
         host: '0.0.0.0',
         port: '3333',
         user: 'root',
         password: '123123',
-        db:'docker'
+        database:'docker'
     }
 };
