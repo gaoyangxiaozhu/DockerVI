ddb0ff24406d7a9b6f118906ae2fdc9f345468ec
gaoyangxiaozhu <gyyzyp@163.com>
2016-06-25 13:51:02 +0800

    add monitor directory from docker_monitor

---
+++ monitor/config.js
@@ -0,0 +1,9 @@
+exports.mysql = {
+    host : 'db',
+    port : '3306',
+    user : 'root',
+    password : 'root'
+};
+exports.dbname = 'docker';
+exports.logDir = './log';
+exports.swarm_add = "http://10.103.242.128:2377/"
