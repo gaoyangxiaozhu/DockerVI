1f737ccfa19df9d5c54f2b6128cc5c76bc2d097a
gaoyangxiaozhu <1456543560@qq.com>
2016-06-25 20:21:25 +0800

    modify development.js

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -4,10 +4,10 @@
 // ==================================
 module.exports = {
     mysql: {
-        host: 'db',
-        port: '3306',
+        host: '0.0.0.0',
+        port: '3333',
         user: 'root',
-        password: 'root',
+        password: '123123',
         database:'docker'
     }
 
