7010d5810abc45a45cb1a82020e63c649c2f2b2f
gaoyangxiaozhu <1456543560@qq.com>
2016-06-26 19:37:31 +0800

    mofidy config.js

--- Deploy/config.js
+++ Deploy/config.js
@@ -3,7 +3,7 @@ config = {
         address: '10.103.242.128:2377'　//docker swarm master 地址
     },
     host:{
-        ip:'10.103.242.128'
+        ip:'10.103.242.128' //host ip for deploy project 
     }
 };
 module.exports = config;
