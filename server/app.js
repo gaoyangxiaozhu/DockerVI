db8f1db4471d343548f09537bb1f3e166beb2431
gaoyangxiaozhu <1456543560@qq.com>
2016-08-05 18:50:57 +0800

    require model js file

--- server/app.js
+++ server/app.js
@@ -12,6 +12,20 @@ var config = require('./config/env');
 var path = require('path');
 var fs = require('fs');
 var mysql = require('mysql');
+var mongoose = require('mongoose');
+
+//连接mongoose数据库
+mongoose.connect(config.mongo.uri, config.mongo.options);
+
+var modelsPath = path.join(__dirname, 'model');
+fs.readdirSync(modelsPath).forEach(function (file) {
+	if (/(.*)\.(js$|coffee$)/.test(file)) {
+		require(modelsPath + '/' + file);
+	}
+});
+
+// 初始化数据
+if(config.seedDB) { require('./config/seed'); }
 
 var app = express();
 var server = require('http').createServer(app);
